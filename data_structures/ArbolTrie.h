b875c02e87292fdc5738462e6dd19cbe886c4629
DarthDipper <vicente.cardenas@alumnos.ucn.cl>
2026-07-03 20:04:06 -0400

    Clase lista
    Ya tenemos creada Arbol Trie

--- data_structures/ArbolTrie.h
+++ data_structures/ArbolTrie.h
@@ -11,4 +11,15 @@ public:
     NodoTrie(char c);
     ~NodoTrie();
 };
+class ArbolTrie {
+private:
+    NodoTrie* raiz;
+    void insertarCadena(const std::string& texto, Cancion* cancion);
+    void insertarSufijos(const std::string& texto, Cancion* cancion);
+public:
+    ArbolTrie();
+    ~ArbolTrie();
+    void insertarCancion(Cancion* cancion);
+    ListaDobleEnlazada<Cancion*> buscar(const std::string& texto);
+};
 #endif
