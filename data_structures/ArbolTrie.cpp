d9d0e9f05d3f13eddcb04a2d6f24b18820faa883
DarthDipper <vicente.cardenas@alumnos.ucn.cl>
2026-07-03 20:15:15 -0400

    Definción inseretar cadena

--- data_structures/ArbolTrie.cpp
+++ data_structures/ArbolTrie.cpp
@@ -15,3 +15,14 @@ ArbolTrie::ArbolTrie(){
 ArbolTrie::~ArbolTrie(){
     delete raiz;
 }
+void ArbolTrie::insertarCadena(const std::string& texto,Cancion* cancion){
+    NodoTrie* actual=raiz;
+    for(unsigned int i=0;i<texto.length();i++){
+        unsigned char indice=(unsigned char)texto[i];
+        if(actual->hijos[indice]==nullptr)
+            actual->hijos[indice]=new NodoTrie(texto[i]);
+        actual=actual->hijos[indice];
+        actual->canciones.agregarAlFinal(cancion);
+    }
+    actual->finPalabra=true;
+}
